db5b085c663242903f0e52d1e86e19125804d3bd
Amani Muhammed <71679579+amanimuhammed@users.noreply.github.com>
2021-09-29 11:33:31 +0530

    Blame me

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -103,7 +103,7 @@ async def channel_receive_handler(bot, broadcast):
             message_id=broadcast.message_id,
             reply_markup=InlineKeyboardMarkup(
                 [
-                    [InlineKeyboardButton("Get Direct Download Link", url=f"https://t.me/AH_File2Link_Bot?start=AbirHasan2005_{str(log_msg.message_id)}")]
+                    [InlineKeyboardButton("Get Direct Download Link", url=f"https://t.me/TG_FileToLinkBot?start=Amanimuhammed_{str(log_msg.message_id)}")]
                 ]
             )
         )
